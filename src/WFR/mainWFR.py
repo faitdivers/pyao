ac814846ecae36b2f54be82212af5240bfde78ab
Jacco van der Spek <me@jac.co.nl>
2014-05-15 15:24:25 +0200

    Fixed the bug of using the wrong parameters

--- src/WFR/mainWFR.py
+++ src/WFR/mainWFR.py
@@ -2,7 +2,7 @@ from numpy import *
 
 def wfr(centroids, params):
 	#Vectorize the centroids in vector s
-	s = centroids.reshape((params['numPupilx']*params['numPupily'],1))
+	s = centroids.reshape((params['noApertx']*params['noAperty'],1))
 	# syntax: ones(shape, dtype=None, order='C')
 	return ones((params['numPupilx'],params['numPupily']))
 
