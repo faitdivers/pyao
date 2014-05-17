8b1410dc7d1aae98858e748317a8955958908534
forcaeluz <m.h.voorsluys@student.tudelft.nl>
2014-05-17 12:55:10 +0200

    Added test for the case of array input / output.

--- src/Control/PIDControllerTest.py
+++ src/Control/PIDControllerTest.py
@@ -6,6 +6,9 @@ Created on May 17, 2014
 import unittest
 import PIDController
 
+from numpy import array
+from numpy import allclose
+
 class Test(unittest.TestCase):
 
 
@@ -21,12 +24,29 @@ class Test(unittest.TestCase):
         self.assertEquals(Kd, controller.Kd);
 
     def testSisoControllerFirstStep(self):
+        """ Tests if the controller reacts as expected in a SISO model
+        
+        """
         Kp = 1.0
         Ki = 0.0
         Kd = 1.1
         controller = PIDController.PIDcontroller(Kp, Ki, Kd)
         output = controller.update(10);
         self.assertEquals(21, output)
+        
+    def testMiMoControllerFirstStep(self):
+        """ Tests if the controller can handle arrays as input and output.
+        
+        """
+        Kp = 1.0
+        Ki = 0.0
+        Kd = 1.1
+        controller = PIDController.PIDcontroller(Kp, Ki, Kd)
+        inputArray = array([1, 2, 3, 4]);
+        expectedOutput = array([2.1, 4.2, 6.3, 8.4])
+        output = controller.update(inputArray);
+        testSucceded = allclose(expectedOutput, output)
+        self.assert_(testSucceded, "Array comparison not close enough")
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testConstructor']
