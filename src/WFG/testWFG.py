e13d131b4e67eea332889cdc2fed3cbd2ca42d4c
larsvl <larsvl@live.nl>
2014-05-20 22:50:01 +0200

    Test the WFG features
    Now also test Noll's indices generation and print them in the console.
    Additionally, the new 3 argument syntax of zernike(...) is used.

--- src/WFG/testWFG.py
+++ src/WFG/testWFG.py
@@ -1,9 +1,9 @@
 from mainWFG import *
 #from main import *
-from zernike import *
 from numpy import *
 from scipy import *
-import math
+from zernike import *
+from nollMap import *
 
 import pylab as p
 #import matplotlib.axes3d as p3
@@ -28,18 +28,17 @@ for obj in paramt :
     count += 1
 
 # Test zernike function
-#   Change the zernike indices u and v here:
-u = 2
-v = 0
+# Change the zernike indices u and v or i here:
+i = 7
+u,v = zernikeIndex(i)
 x = linspace(-1,1,paramt['numImagx'])
 y = linspace(-1,1,paramt['numImagy'])
 X,Y = meshgrid(x,y)
 R,T = cart2pol(X,Y)
-
-Zg = zernike(u,v,R,T)
+Zg = zernike(R,T,i)
 title = 'Zernike Polynomial: u = %d, v = %d' %(u,v)
 
-#   Plot results in a surface plot
+# Plot results in a surface plot
 fig = p.figure();
 ax = p3.Axes3D(fig)
 ax.plot_surface(X,Y,Zg, rstride=1, cstride=1, cmap='jet')
@@ -47,6 +46,17 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel(title)
 p.show()
+
+# Test Noll's mapping
+ul = 0
+str = ""
+for n in range(1,81):
+    u,v = zernikeIndex(n)
+    if ul != u:
+        print(str)
+        ul = u
+        str = ""
+    str = str + "%d: (%d,%d) " %(n,u,v)
 
 # Test the total wfg routine here
 #   DO NOT USE RESULTS! It will always be ones
