c19b640c853a8a716d28c99313df3fc34990ae97
Jacco van der Spek <jaccovdspek@gmail.com>
2014-05-22 10:01:02 +0200

    test file and there are still some bugs, creation of phase_id is going wrong

--- src/WFR/mainWFR.py
+++ src/WFR/mainWFR.py
@@ -19,7 +19,35 @@ def wfr(centroids, params):
 				phase_id[i*x_dim+j+1,0] = 1
 				phase_id[(i+1)*x_dim+j,0] = 1
 				phase_id[(i+1)*x_dim+j+1,0] = 1
-				
+	
+	#Create phase_num vector
+	teller = 0
+	phase_num = zeros(((x_dim+1)*(y_dim+1),1))
+	for i in range(0,x_dim+1):
+		for j in range(0,y_dim+1):
+			#Check if the phase_id is non zero
+			if phase_id[i*(x_dim+1)+j] != 0:
+				phase_num[i*(x_dim+1)+j] = teller
+				teller += 1
+	
+	#Create G matrix
+	#For clarity an actual matrix shape is used
+	G = zeros((2*number_slopes, teller))
+	counter = 0
+	for i in range(0,x_dim):
+		for j in range(0,y_dim):
+			if s[i*x_dim+j,0] != 0 or s[i*x_dim+j+number_slopes,0] != 0:
+				#For s_x
+				G[counter, phase_num[i*x_dim+j]] = -1
+				G[counter, phase_num[i*x_dim+j+1]] = -1
+				G[counter, phase_num[(i+1)*x_dim+j]] = 1
+				G[counter, phase_num[(i+1)*x_dim+j+1]] = 1
+				#For s_y
+				G[counter+number_slopes, phase_num[i*x_dim+j]] = -1
+				G[counter+number_slopes, phase_num[(i+1)*x_dim+j]] = -1
+				G[counter+number_slopes, phase_num[i*x_dim+j+1]] = 1
+				G[counter+number_slopes, phase_num[(i+1)*x_dim+j+1]] = 1
+				counter += 1
 	
 	# syntax: ones(shape, dtype=None, order='C')
 	return ones((params['numPupilx'],params['numPupily']))
---
+++ src/WFR/testWFR.py
@@ -0,0 +1,61 @@
+from mainWFR import *
+
+def setup_params():
+	""" Set-up the simulation parameters
+
+	Setup the parameters used for all steps of the simulation.
+	This includes parameters for the sensor, for the actuator and
+	possibly other necessary simulation configuration.
+
+	Returns:
+		Multiple dicts, containing the parameters and their values.
+	"""
+
+	paramsSensor = {
+	# number of samples in the pupil plane
+	'numPupilx' : 100,
+	'numPupily' : 100,
+	# number of samples in the imaging plane(s)
+	'numImagx' : 100,
+	'numImagy' : 100,
+	# number of apertures in the wfs
+	'noApertx': 3,
+	'noAperty': 3,
+	# focal distance, pixel length, sizes/diameters of the apertures, ...
+	}
+
+	paramsActuator = {
+	# number of actuators
+	'numActx' : 8,
+	'numActy' : 8,
+	# parameters to characterize influence function
+	}
+
+	simulationParameters = {
+	'frequency' : 10,       # Frequency of the simulation in Hertz
+	'time' : 10             # Simulated time in seconds
+	}
+
+	# other sets of parameters may be defined if necessary
+
+	# Encapsulate all the parameter dicts
+	parameters = {
+	'Sensor' : paramsSensor,
+	'Actuator' : paramsActuator,
+	'Simulation' : simulationParameters
+	}
+
+	return parameters
+
+def test_run():
+	# Get parameters.
+	parameters = setup_params()
+	sensorParameters = parameters['Sensor'];
+	actuatorParameters = parameters['Actuator'];
+	simulationParameters = parameters['Simulation'];
+
+	centroids = ones((sensorParameters['noApertx']*sensorParameters['noAperty']*2,1))
+	wfRec = wfr(centroids, sensorParameters)
+	return
+
+test_run()
