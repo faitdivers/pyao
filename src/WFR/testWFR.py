81821e7a026c7c8823c4d72081d1836b808fb657
Jacco van der Spek <jaccovdspek@gmail.com>
2014-05-24 10:55:47 +0200

    removed all unnecessary parameter setups

--- src/WFR/testWFR.py
+++ src/WFR/testWFR.py
@@ -3,10 +3,6 @@ from mainWFR import *
 def setup_params():
 	""" Set-up the simulation parameters
 
-	Setup the parameters used for all steps of the simulation.
-	This includes parameters for the sensor, for the actuator and
-	possibly other necessary simulation configuration.
-
 	Returns:
 		Multiple dicts, containing the parameters and their values.
 	"""
@@ -24,25 +20,9 @@ def setup_params():
 	# focal distance, pixel length, sizes/diameters of the apertures, ...
 	}
 
-	paramsActuator = {
-	# number of actuators
-	'numActx' : 8,
-	'numActy' : 8,
-	# parameters to characterize influence function
-	}
-
-	simulationParameters = {
-	'frequency' : 10,       # Frequency of the simulation in Hertz
-	'time' : 10             # Simulated time in seconds
-	}
-
-	# other sets of parameters may be defined if necessary
-
 	# Encapsulate all the parameter dicts
 	parameters = {
 	'Sensor' : paramsSensor,
-	'Actuator' : paramsActuator,
-	'Simulation' : simulationParameters
 	}
 
 	return parameters
@@ -51,8 +31,6 @@ def test_run():
 	# Get parameters.
 	parameters = setup_params()
 	sensorParameters = parameters['Sensor'];
-	actuatorParameters = parameters['Actuator'];
-	simulationParameters = parameters['Simulation'];
 
 	centroids = ones((sensorParameters['noApertx']*sensorParameters['noAperty']*2,1))
 	wfRec = wfr(centroids, sensorParameters)
