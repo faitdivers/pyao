bbcbe793a4ad745eac01f682cb645ce0232ff7d9
forcaeluz <m.h.voorsluys@student.tudelft.nl>
2014-05-24 17:10:07 +0200

    Added a latency buffer class, and unit-tests. The latency buffer is a circular buffer, which is meant to introduce a delay between input and output.

---
+++ src/Simulation/LatencyBufferTest.py
@@ -0,0 +1,40 @@
+'''
+Created on May 24, 2014
+
+@author: forcaeluz
+'''
+import unittest
+import LatencyBuffer
+import numpy as np
+
+class TestLatencyBuffer(unittest.TestCase):
+
+
+    def test_constructor_datasize(self):
+        dataSize = (10, 10)
+        myBuffer = LatencyBuffer.LatencyBuffer(10, dataSize)
+        data = myBuffer.get_stored_data()
+        data_size = len(data)
+        self.assertEqual(1000, data_size)
+        
+    def test_constructor_emptydata(self):
+        dataSize = (10, 10)
+        myBuffer = LatencyBuffer.LatencyBuffer(10, dataSize)
+        data = myBuffer.get_stored_data()
+        result = all(x == 0 for x in data)
+        self.assertTrue(result, "Not all the elements are zeros")
+
+    def test_update_data(self):
+        dataSize = (10,10)
+        myBuffer = LatencyBuffer.LatencyBuffer(10, dataSize)
+        dataSet = np.ones([10,10])
+        myBuffer.update(dataSet)
+        data = myBuffer.get_stored_data()
+        result1 = all(x == 1 for x in data[range(100)])
+        result2 = all(x == 0 for x in data[range(100, 1000)])
+        self.assertTrue(result1, "Not all the first 100 elements are ones")
+        self.assertTrue(result2, "Not all the last 900 elements are zeros")
+        
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
