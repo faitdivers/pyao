8fc8fecdfb82ba923192246b2e93a20e42dfc0e1
forcaeluz <m.h.voorsluys@student.tudelft.nl>
2014-05-25 13:25:45 +0200

    Added documentation.

--- src/Simulation/LatencyBuffer.py
+++ src/Simulation/LatencyBuffer.py
@@ -3,16 +3,27 @@ Created on May 22, 2014
 
 @author: forcaeluz
 '''
-#from collections import deque
 import numpy as np
 import operator as op
 
 
 class LatencyBuffer():
-
+    """ LatencyBuffer class: A ring buffer to simulate latency
+        The LatencyBuffer is a ring-buffer created to introduce
+        a latency into the simulation.
+    """
     def __init__(self, bufferLength, data_shape):
         """ Constructs the LatencyBuffer
 
+            The constructor pre-allocate the whole buffer, initializes it to 0
+            and set the write index to 0.
+
+        Args:
+            bufferLenght: Number of data-frames that the buffer should store.
+            data_shape: Size of the data coming in multiple dimensions.
+                Example: (10, 1) is a vector of 10 elements
+                         (10, 10) is a 10x10 matrix
+                         (10, 10, 10) is a 10x10x10 3D matrix
         """
         self.data_shape = data_shape
         self.data_framesize = reduce(op.mul, data_shape)
@@ -20,16 +31,38 @@ class LatencyBuffer():
         self.index = 0
 
     def update(self, data):
+        """ Updates the content of the buffer
+
+        Args:
+            data: The data to be inserted into the buffer.
+
+        Returns:
+            The oldest stored data.
+
+        TODO:
+            - Better error handling
+            - Reshape data output
+        """
         self._set_newest(data)
         return self._get_oldest()
 
     def _get_oldest(self):
+        """ Returns the oldest data stored
+
+        TODO:
+            - Reshape data output.
+        """
         r_index = (self.index + np.arange(self.data_framesize))
         r_index = r_index % self.data.size
         output = self.data[r_index]
         return output
 
     def _set_newest(self, data):
+        """ Set the data that should be stored in the buffer at write index
+
+        TODO:
+            - Better error handling.
+        """
         if data.shape == self.data_shape:
             x_index = (self.index + np.arange(data.size)) % self.data.size
             self.data[x_index] = data
@@ -38,4 +71,7 @@ class LatencyBuffer():
             print("ERROR: Data shape is not correct")
 
     def get_stored_data(self):
+        """ Returns all the content of the buffer.
+
+        """
         return self.data
