5ef8f9ef68807ce589b090fd823efb5d181abf20
Jacco van der Spek <jaccovdspek@gmail.com>
2014-06-06 23:02:26 +0200

    implementation of A matrix

--- src/WFR/zernikeModalMatrix.py
+++ src/WFR/zernikeModalMatrix.py
@@ -2,4 +2,19 @@ from numpy import *
 
 def zernikeModalMatrix(N, l):
 	
+	# domain defenition
+	gridx = linspace(-1,1,N)
+	x,y = meshgrid(gridx, gridx)
+	r = sqrt(x**2+y**2)
+	
+	A = zeros((N,N)) #initialize A
+	idx = r<1
+	A[idx] = l #every entry in idx which <1 gets the value l
+	
+	##Building the modal matrix
+	# X derivative
+	
+	# Y derivative
+
+	
 	return 0
