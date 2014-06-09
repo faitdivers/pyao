e2c871c87c742c32f5ca368f52d027cfb8bd1562
Yudha Pane <yudhapane@yahoo.com>
2014-06-09 23:57:46 +0200

    fixing conflicst

--- src/WFR/mainWFR.py
+++ src/WFR/mainWFR.py
@@ -1,8 +1,67 @@
 from numpy import *
 
 def wfr(centroids, params):
+<<<<<<< HEAD
 	#Vectorize the centroids in vector s
 	#s = centroids.reshape((params['numPupilx']*params['numPupily'],1))
+=======
+	""" Compute the reconstructed wavefront
+
+	Reconstruct the wavefront from the received slopes
+	Expected input: [s_x(0,0),...,s_x(0,N),...,s_x(M,0),...,s_x(M,N),s_y(0,0),...,s_y(0,N),...,s_y(M,0),...,s_y(M,N)]^T
+	
+	Returns:
+		The phi matrix in the form [phi(0,0),...,phi(0,P),...,phi(Q,0),...,phi(P,Q)]^T
+	"""
+	#Get dimensions
+	x_dim = params['noApertx']
+	y_dim = params['noAperty']
+	number_slopes = x_dim*y_dim
+	
+	s = centroids
+	
+	#Create a phase_id matrix
+	phase_id = zeros(((x_dim+1),(y_dim+1)),dtype=int)
+	for i in range(0,x_dim):
+		for j in range(0,y_dim):
+			#Check if the slope at s(i,j) is non zero
+			#Put a 1 at each place of phi we need
+			if s[i*x_dim+j,0] != 0 or s[i*x_dim+j+number_slopes,0] != 0:
+				phase_id[i,j] = 1
+				phase_id[i,j+1] = 1
+				phase_id[i+1,j] = 1
+				phase_id[i+1,j+1] = 1
+	
+	#Create phase_num matrix
+	teller = 0
+	phase_num = zeros(((x_dim+1),(y_dim+1)),dtype=int)
+	for i in range(0,x_dim+1):
+		for j in range(0,y_dim+1):
+			#Check if the phase_id is non zero
+			if phase_id[i,j] != 0:
+				phase_num[i,j] = teller
+				teller += 1
+	
+	#Create G matrix
+	#For clarity an actual matrix shape is used
+	G = zeros((2*number_slopes, teller),dtype=int)
+	counter = 0
+	for i in range(0,x_dim):
+		for j in range(0,y_dim):
+			if s[i*x_dim+j,0] != 0 or s[i*x_dim+j+number_slopes,0] != 0:
+				#For s_x
+				G[counter, phase_num[i,j]] = -1
+				G[counter, phase_num[i,j+1]] = -1
+				G[counter, phase_num[i+1,j]] = 1
+				G[counter, phase_num[i+1,j+1]] = 1
+				#For s_y
+				G[counter+number_slopes, phase_num[i,j]] = -1
+				G[counter+number_slopes, phase_num[i+1,j]] = -1
+				G[counter+number_slopes, phase_num[i,j+1]] = 1
+				G[counter+number_slopes, phase_num[i+1,j+1]] = 1
+				counter += 1
+	print('G = \n{}'.format(G))	
+>>>>>>> master
 	# syntax: ones(shape, dtype=None, order='C')
 	return ones((params['numPupilx'],params['numPupily']))
 
